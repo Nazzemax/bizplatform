5c04d29a0fddcb281a2308ba16b3731a9d8aebec
Nazzemax <hvan1998@inbox.ru>
2024-04-07 01:49:22 +0600

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import  { About } from '../widgets/AboutThePlatform/About'
 import styles from "./page.module.css";
-import { Price } from "@/widgets/Price";
+
 
 export default function Home() {
   return (
